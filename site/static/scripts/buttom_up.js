e16a0a203d30b0f5ca54701bf8c89528bac4a392
uvaprol <120366489+uvaprol@users.noreply.github.com>
2024-04-05 00:43:29 +0300

    Add files via upload

---
+++ site/static/scripts/buttom_up.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById('up')
+let timerId = setTimeout(()=>{btn.style.display = 'none'}, 500)
+
+function button_up_hide(){
+    btn.style.animation = 'unvisible 1.5s forwards'
+    timerId = setTimeout(()=>{btn.style.display = 'none'}, 500)
+}
+
+window.onwheel = (e) =>{
+    if (e.deltaY >= 0 || $(window).scrollTop() <= 100){
+        button_up_hide()
+    }
+    else{
+        btn.style.animation = 'visible 1.5s forwards'
+        clearTimeout(timerId)
+        btn.style.display = 'block'
+    }
+}
